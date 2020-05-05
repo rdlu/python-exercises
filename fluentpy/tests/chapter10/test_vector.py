9a2b7030984d74257dbebd6886462c287759ccc6
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-05-05 13:18:45 -0300

    playing with mark parametrize 2

--- fluentpy/tests/chapter10/test_vector.py
+++ fluentpy/tests/chapter10/test_vector.py
@@ -10,10 +10,6 @@ class TestVector:
     v4 = v.Vector([1, 1])
     zero_vectors = [v.Vector([0] * i) for i in (0, 3, 1000)]
 
-    @pytest.fixture(params=zero_vectors)
-    def fixture_zero_vectors(self, request):
-        return request.param
-
     def test_iter_matching(self):
         x, y = self.v1
         assert (x, y) == (3.1, 4.2), \
@@ -75,9 +71,10 @@ class TestVector:
         assert hash(self.v1) != hash(self.v3), \
             'hash must be different for different components'
 
-    def test_abs_zero(self, fixture_zero_vectors):
-        assert abs(fixture_zero_vectors) == 0, \
-                'abs is 1 on zeroed components {}'.format(fixture_zero_vectors)
+    @pytest.mark.parametrize("vector", zero_vectors)
+    def test_abs_zero(self, vector):
+        assert abs(vector) == 0, \
+                'abs is 1 on zeroed components {}'.format(vector)
 
     def test_abs_non_zero(self):
         assert abs(self.v2) == 7.0710678118654755, \
@@ -107,8 +104,9 @@ class TestVector:
         assert format(v.Vector((-1, -1)), '0.2fh') == '<1.41, 3.93>', \
             'n-sphere format with negative numbers, angles use a different rule'
 
-    def test_bool_false(self, fixture_zero_vectors):
-        assert not bool(fixture_zero_vectors), \
+    @pytest.mark.parametrize("vector", zero_vectors)
+    def test_bool_false(self, vector):
+        assert not bool(vector), \
             'bool conversion must evaluate False on zero vectors'
 
     def test_bool_true(self):
